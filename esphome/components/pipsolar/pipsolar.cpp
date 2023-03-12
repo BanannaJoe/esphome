61753024d026a0b19f85ac7288aaad6e09ae636b
BanannaJoe <59600975+BanannaJoe@users.noreply.github.com>
2023-03-12 20:57:57 +0100

    add^

--- esphome/components/pipsolar/pipsolar.cpp
+++ esphome/components/pipsolar/pipsolar.cpp
@@ -867,7 +867,7 @@ void Pipsolar::dump_config() {
 }
 void Pipsolar::update() {}
 
-void Pipsolar::add_polling_command_(const char *command, ENUMPollingCommand polling_command) {
+void Pipsolar::add_polling_command_(const char ^*command, ENUMPollingCommand polling_command) {
   for (auto &used_polling_command : this->used_polling_commands_) {
     if (used_polling_command.length == strlen(command)) {
       uint8_t len = strlen(command);
